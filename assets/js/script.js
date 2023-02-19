d9e0aebdc058be51d3b9fbbdb3f40b2429f63887
OES <eunseok0722@gmail.com>
2023-02-19 14:06:05 +0900

    js 스크립트추가

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
-'use strict'; 
+"use strict";
 
-console.log('script start');
+console.log("script start");
+
+const d2Wrap = document.getElementsByClassName("d2-list-wrap");
+const subHeader = document.querySelector(".sub-header");
+const mainHeader = document.querySelector(".main-header-wrap");
+const d1Item = document.getElementsByClassName("d1-item");
+
+for (let i = 0; i < d1Item.length; i++) {
+  d1Item[i].addEventListener("mouseenter", () => {
+    for (let j = 0; j < d1Item.length; j++) {
+      d2Wrap[j].classList.remove("active");
+    }
+    d2Wrap[i].classList.add("active");
+  });
+}
+subHeader.addEventListener("mouseleave", () => {
+  for (let i = 0; i < d1Item.length; i++) {
+    d2Wrap[i].classList.remove("active");
+  }
+});
+
+mainHeader.addEventListener("mouseenter", () => {
+  mainHeader.classList.add("active");
+});
+mainHeader.addEventListener("mouseleave", () => {
+  mainHeader.classList.remove("active");
+});
+
+const d3Item = document.querySelectorAll(".d3-item");
+const itemActive = (number) => {
+  for (let i = 0; i < d3Item.length; i++) {
+    d3Item[i].classList.remove("active");
+  }
+  d3Item[number].classList.add("active");
+};
+const itemInActive = (number) => {
+  for (let i = 0; i < d3Item.length; i++) {
+    d3Item[i].classList.remove("active");
+  }
+};
+
